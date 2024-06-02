5783251a5d54f5e4760386f950a248adcf3c8736
itsCasualss <austinpetit@gmail.com>
2024-06-02 21:57:39 +0000

    Lucinda Potter

--- main.cs
+++ main.cs
@@ -1,7 +1,10 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World, It's Austin Here!");
+class Program{
+  public static void Main(string[] args){
+
+    System.Console.WriteLine(ConcatName ("Lucinda Potter", "6/24/1992","000-000-0101","000-000-0189"));
+  static string ConcatName (string name, string dob, string work, string cell){
+    return name + "\n" + dob + "\n" + work + "\n"+ cell;}
+    }
   }
-}
