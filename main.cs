40c722d24470ea9d8e2d5a338b9faab91a0e7bd6
itsCasualss <austinpetit@gmail.com>
2024-06-03 00:58:29 +0000

    Module 1.2 , songs added

--- main.cs
+++ main.cs
@@ -3,19 +3,21 @@ using System;
 class Program{
   public static void Main(string[] args){
 
-    System.Console.WriteLine(UnitTestOne ("Lucinda Potter", "6/24/1992","000-000-0101","000-000-0189"));
-  static string UnitTestOne (string name, string dob, string work, string cell){
-    return name + "\n" + dob + "\n" + work + "\n"+ cell + "\n";}
+    System.Console.WriteLine(SongOne ("Oh, well imagine", "As I'm pacing the pews in a church corridor","And I can't help but to hear","No, I can't help but to hear an exchanging of words..."));
+  static string SongOne (string lineone, string linetwo, string linethree, string linefour){
+    return lineone + "\n" + linetwo + "\n" + linethree + "\n"+ linefour + "\n";}
     
-    System.Console.WriteLine(UnitTestTwo("John Smith","8/15/1978","000-111-0101","000-111-0189"));
-  static string UnitTestTwo (string name, string dob, string work, string cell){
-    return name + "\n" + dob + "\n" + work + "\n"+ cell + "\n";}
-    System.Console.WriteLine(UnitTestThree("Chris Johnson","12/01/1987","000-222-0101","000-222-0189"));
-    static string UnitTestThree (string name, string dob, string work, string cell){
-      return name + "\n" + dob + "\n" + work + "\n"+ cell + "\n";}
-    System.Console.WriteLine(UnitTestFour("Robert Hall","2/27/1949","000-444-0101","000-444-0189"));
-    static string UnitTestFour (string name, string dob, string work, string cell){
-      return name + "\n" + dob + "\n" + work + "\n"+ cell;}
+    System.Console.WriteLine(SongTwo("Oh-oh-oh-oh-oh,", "oh-oh-oh-oh,","oh-oh-oh","Caught in a bad romance..."));
+    static string SongTwo (string lineone, string linetwo, string linethree, string linefour){
+      return lineone +"\n"+ linetwo +"\n"+ linethree + "\n"+ linefour + "\n";}
+    
+    System.Console.WriteLine(SongThree("Coming out of my cage", "And I've been doing just fine","Gotta gotta be down","Because I want it all"));
+    static string SongThree (string lineone, string linetwo, string linethree, string linefour){
+      return lineone + "\n" + linetwo + "\n" + linethree + "\n"+ linefour + "\n";}
+    
+    System.Console.WriteLine(SongFour("Now, this is a story all about how", "My life got flipped-turned upside down","And I'd like to take a minute","Just sit right there", "I'll tell you how I became the prince of a town called Bel-Air"));
+    static string SongFour (string lineone, string linetwo, string linethree, string linefour, string linefive){
+      return lineone + "\n" + linetwo + "\n" + linethree + "\n"+ linefour + "\n" + linefive;}
     }
-  
+
   }
