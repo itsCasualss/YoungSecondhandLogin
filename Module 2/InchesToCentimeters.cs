b59429428fc3b33ab4b0335eb13cd8e3bd706bbf
itsCasualss <austinpetit@gmail.com>
2024-06-14 22:31:50 +0000

    Created module 2.1 InchesToCentimeters

---
+++ Module 2/InchesToCentimeters.cs
@@ -0,0 +1,29 @@
+public class InchesToCentimeters : IAssignment
+{
+    public void Run()
+    {
+        { double cm; // creating a constant variable
+            double num01;
+            double num02;
+            double num03;
+            double num04;
+            
+            cm = 2.54;
+            num01 = 5;
+            num02 = 50;
+            num03 = -15;
+            num04 = 15994;
+
+            System.Console.WriteLine(num01 + " inches is equal to " + num01 * cm + " centimeters");
+            System.Console.WriteLine(num02 + " inches is equal to " + num02 * cm + " centimeters");
+            System.Console.WriteLine(num03 + " inches is equal to " + num03 * cm + " centimeters");
+            System.Console.WriteLine(num04 + " inches is equal to " + num04 * cm + " centimeters");
+    
+    
+    
+    
+        
+    
+            }  
+    }
+} 
