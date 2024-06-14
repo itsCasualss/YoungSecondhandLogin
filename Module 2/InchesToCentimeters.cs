a73faa0ba70cfebc4933a3ac448576db5ee5a344
itsCasualss <austinpetit@gmail.com>
2024-06-14 22:35:18 +0000

    Cleaned up the code for InchesToCentimeters

--- Module 2/InchesToCentimeters.cs
+++ Module 2/InchesToCentimeters.cs
@@ -2,27 +2,17 @@ public class InchesToCentimeters : IAssignment
 {
     public void Run()
     {
-        { double cm; // creating a constant variable
-            double num01;
-            double num02;
-            double num03;
-            double num04;
-            
-            cm = 2.54;
-            num01 = 5;
-            num02 = 50;
-            num03 = -15;
-            num04 = 15994;
+        { 
+            double cm = 2.54; // creating constant variable
+            double num01 = 5;
+            double num02 = 50;
+            double num03 = -15;
+            double num04 = 15994;
 
             System.Console.WriteLine(num01 + " inches is equal to " + num01 * cm + " centimeters");
             System.Console.WriteLine(num02 + " inches is equal to " + num02 * cm + " centimeters");
             System.Console.WriteLine(num03 + " inches is equal to " + num03 * cm + " centimeters");
             System.Console.WriteLine(num04 + " inches is equal to " + num04 * cm + " centimeters");
-    
-    
-    
-    
-        
     
             }  
     }
